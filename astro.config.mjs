f13c1240f2ba3938c804c97f02097c89c1eab605
Klender Carrasco <marcosklender@gmail.com>
2024-07-08 11:01:03 -0500

    Update md file to mdx extension
    Update content and meta data.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,9 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
+			defaultLocale: 'en',
 			title: 'JSONLinks',
+			description: 'Start creating your own links page for free! A repository where you will find easy to implement links pages that fit your needs.',
 			social: {
 				github: 'https://github.com/MarcosKlender/JSONLinks',
 			},
